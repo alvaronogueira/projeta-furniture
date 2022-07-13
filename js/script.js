c4f9b3a79bf3ab58d5388f28862e9373314fb8d0
alvaronogueira <alvaronogueira29@gmail.com>
2022-07-13 15:43:11 -0300

    update projeta

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById("menu");
+
+function barraDeMenu() {
+    if(menu.style.display == "none"){
+        menu.style.display = "flex"
+    }else{
+        menu.style.display = "none"
+    }
+}
